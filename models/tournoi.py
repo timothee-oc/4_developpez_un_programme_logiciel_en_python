01ee548b2a4cce12b3de9153aab5915ce8596064
timothee-oc <timothee.oc@proton.me>
2023-10-06 16:48:26 +0200

    correction

--- models/tournoi.py
+++ models/tournoi.py
@@ -1,11 +1,6 @@
-from tour import Tour
-from match import Match
-from joueur import Joueur
-import json
+from models.tour import Tour
+from models.match import Match
 from random import shuffle
-
-
-PATH_TO_JSON = "data/tournaments/tournoi.json"
 
 
 class Tournoi:
@@ -43,12 +38,3 @@ class Tournoi:
                               reverse=True)
         for joueur in self.joueurs:
             print(joueur.prenom, joueur.points)
-
-
-with open(PATH_TO_JSON, 'r', encoding='utf-8') as json_tournoi:
-    donnees_json = json.load(json_tournoi)
-
-joueurs = [Joueur(joueur) for joueur in donnees_json["joueurs"]]
-print([joueur.prenom for joueur in joueurs])
-tournoi1 = Tournoi(joueurs)
-tournoi1.run()
